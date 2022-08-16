1309dcebfcdaf60d7fc4494c3881fb9f254ead10
YajneshVishwakarma <75421738+YajneshVishwakarma@users.noreply.github.com>
2022-08-16 23:28:12 +0530

    Add files via upload

---
+++ Student_Driver.java
@@ -0,0 +1,16 @@
+public class Student_Driver {
+    public static void main(String[] var0) {
+        System.out.println("Program Started");
+        System.out.println("=============================");
+        new Student("Yash");
+        System.out.println("=============================");
+        new Student("Yash", 121L);
+        System.out.println("=============================");
+        new Student("Yash", 12221L, "Mijar");
+        System.out.println("=============================");
+        new Student("Yash", 12221L, "Mijar", 1888.0D);
+        System.out.println("=============================");
+        System.out.println("Program Ended");
+
+    }
+}

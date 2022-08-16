193454108eadc29a358423d0bfef2b287513abd6
YajneshVishwakarma <75421738+YajneshVishwakarma@users.noreply.github.com>
2022-08-16 23:29:11 +0530

    Update Student_Driver.java

--- Student_Driver.java
+++ Student_Driver.java
@@ -1,3 +1,47 @@
+public class Student {
+        String name;
+        long id;
+        String place;
+        double fees;
+
+    Student(String var1) {
+            this.name = var1;
+            System.out.println("Program Started");
+        }
+
+    Student(String var1, long var2) {
+            this.name = var1;
+            this.id = var2;
+            System.out.println(var1);
+            System.out.println(var2);
+        }
+
+    Student(String var1, long var2, String var4) {
+            this.name = var1;
+            this.id = var2;
+            this.place = var4;
+            System.out.println(var1);
+            System.out.println(var2);
+            System.out.println(var4);
+        }
+
+    Student(String var1, long var2, String var4, double var5) {
+            this.name = var1;
+            this.id = var2;
+            this.place = var4;
+            this.fees = var5;
+            System.out.println(var1);
+            System.out.println(var2);
+            System.out.println(var4);
+            System.out.println(var5);
+
+
+
+    }
+
+}
+
+
 public class Student_Driver {
     public static void main(String[] var0) {
         System.out.println("Program Started");
